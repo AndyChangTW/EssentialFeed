e67b6a803ab1bf3c930378a3d4a8fd1f35073fe6
Andy Chang <andy.chang@ai3.cloud>
2022-09-28 20:49:51 +0800

    Create FeedItem

---
+++ Feed Case Study/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed Case Study
+//
+//  Created by Andy Chang on 2022/9/28.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
