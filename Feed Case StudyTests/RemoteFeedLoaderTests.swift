40f914849b372b5e5229accb9d3907b587678597
Andy Chang <andy.chang@ai3.cloud>
2022-09-29 21:30:54 +0800

    removed stubbing and improve readability with helper method

--- Feed Case StudyTests/RemoteFeedLoaderTests.swift
+++ Feed Case StudyTests/RemoteFeedLoaderTests.swift
@@ -39,11 +39,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        client.error = NSError(domain: "Test", code: 0)
         
         var capturedErrors = [RemoteFeedLoader.Error]()
-        
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -57,12 +58,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 
