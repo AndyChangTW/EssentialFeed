3a50ba9e002b7b0ca595349ddea705f0a23f2608
Andy Chang <andy.chang@ai3.cloud>
2022-10-04 22:09:58 +0800

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ Feed Case StudyTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,48 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  Feed Case StudyTests
+//
+//  Created by Andy Chang on 2022/10/4.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in
+            
+        }
+    }
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://a-given-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+        
+    }
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+
+}
