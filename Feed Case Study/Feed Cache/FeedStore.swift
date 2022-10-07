079a84f10fae2f3e19853468de01e47aacb0bcbe
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-07 20:48:40 +0800

    Move `FeedStore` to its own file

---
+++ Feed Case Study/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  FeedCaseStudy
+//
+//  Created by Andy Chang on 2022/10/7.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
