0c9d8b1b426de31e06c994ad2972eaa2cba12fc0
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-07 14:13:18 +0800

    Extract system under test creation to a factory method to protect tests against breaking changes and add memory leak tracking

--- Feed Case StudyTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ Feed Case StudyTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -30,20 +30,26 @@ class FeedStore {
 final class CacheFeedUseCaseTests: XCTestCase {
 
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestsCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
         
         sut.save(items)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
     }
 
     
