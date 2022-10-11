09dd348984cff513b2891abad672dc88aa4cf4c1
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-12 00:02:59 +0800

    Dispatch `CodableFeedStore` operations in a serial background queue to avoid blocking clients

--- Feed Case StudyTests/Feed Cache/CodableFeedStoreTests.swift
+++ Feed Case StudyTests/Feed Cache/CodableFeedStoreTests.swift
@@ -231,7 +231,7 @@ final class CodableFeedStoreTests: XCTestCase {
             deletionError = error
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 2.0)
         return deletionError
     }
     
