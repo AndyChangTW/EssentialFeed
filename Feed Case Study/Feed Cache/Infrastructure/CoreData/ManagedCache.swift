47053d9fcf048de1e3f395f3d64af4959e6cf0a4
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-13 00:28:35 +0800

    Extract CoreData managed model classes into separate files

---
+++ Feed Case Study/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,34 @@
+//
+//  ManagedCache.swift
+//  FeedCaseStudy
+//
+//  Created by Andy Chang on 2022/10/13.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func delete(in context: NSManagedObjectContext) throws {
+        try find(in: context).map(context.delete)
+        try context.save()
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+}
