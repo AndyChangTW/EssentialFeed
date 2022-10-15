661ba9ad3db94afa45a6c65792843dd977eb5bef
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-15 15:34:53 +0800

    Add separate CI schemes for macOS and iOS as we now have an iOS-specific target that should not be tested on macOS

---
+++ FeedCaseStudyiOS/FeedCaseStudyiOS.h
@@ -0,0 +1,18 @@
+//
+//  FeedCaseStudyiOS.h
+//  FeedCaseStudyiOS
+//
+//  Created by Andy Chang on 2022/10/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedCaseStudyiOS.
+FOUNDATION_EXPORT double FeedCaseStudyiOSVersionNumber;
+
+//! Project version string for FeedCaseStudyiOS.
+FOUNDATION_EXPORT const unsigned char FeedCaseStudyiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedCaseStudyiOS/PublicHeader.h>
+
+
