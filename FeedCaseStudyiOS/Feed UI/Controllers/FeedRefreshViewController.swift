aff56f8abf6cfdf0a6e71c54618f6ee7c135ffbe
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-19 21:23:07 +0800

    Move `FeedRefreshViewController` to its own file

---
+++ FeedCaseStudyiOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedRefreshViewController.swift
+//  FeedCaseStudyiOS
+//
+//  Created by Andy Chang on 2022/10/19.
+//
+
+import UIKit
+import FeedCaseStudy
+
+public class FeedRefreshViewController: NSObject {
+    private (set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private let feedLoader: FeedLoader
+    
+    public init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
