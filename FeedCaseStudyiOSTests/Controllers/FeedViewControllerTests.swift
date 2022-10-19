9a14dbae8175846116b29e1ab5c6940b84531278
AndyChangTW <kawasakihahaha@hotmail.com>
2022-10-19 20:49:34 +0800

    Extract custom assertions to a separate file

--- FeedCaseStudyiOSTests/Controllers/FeedViewControllerTests.swift
+++ FeedCaseStudyiOSTests/Controllers/FeedViewControllerTests.swift
@@ -10,7 +10,7 @@ import UIKit
 import FeedCaseStudy
 import FeedCaseStudyiOS
 
-final class FeedCaseStudyiOSTests: XCTestCase {
+final class FeedViewControllerTests: XCTestCase {
     
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut, loader) = makeSUT()
@@ -266,31 +266,5 @@ final class FeedCaseStudyiOSTests: XCTestCase {
     
     private func makeImage(id: UUID = UUID(), description: String? = nil, location: String? = nil, url: URL = URL(string: "http://a-url.com")!) -> FeedImage {
         return FeedImage(id: id, description: description, location: location, url: url)
-    }
-    
-    private func assertThat(_ sut: FeedViewController, toRender feed: [FeedImage], file: StaticString = #filePath, line: UInt = #line) {
-        guard sut.numberOfRenderedFeedImageViews() == feed.count else {
-            XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
-            return
-        }
-        
-        feed.enumerated().forEach { index, image in
-            assertThat(sut, hasViewConfiguredFor: image, at: index, file: file, line: line)
-        }
-    }
-    
-    private func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #filePath, line: UInt = #line) {
-        let view = sut.feedImageView(at: index)
-        guard let cell = view as? FeedImageCell else {
-            XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
-            return
-        }
-        
-        let shouldLocationBeVisible = (image.location != nil)
-        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for image view at index (\(index))", file: file, line: line)
-        
-        XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image  view at index (\(index))", file: file, line: line)
-        
-        XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index (\(index)", file: file, line: line)
     }
 }
