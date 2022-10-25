84fe2cde46208b971a3c8f09905507894a9f5008
Andy Chang <andy.chang@ai3.cloud>
2022-10-25 23:47:26 +0800

    Move memory management to Composer layer. The `FeedPresenter` shouldn't have to know or handle their dependencies lifetime.

--- FeedCaseStudyiOS/Feed UI/Composer/FeedUIComposer.swift
+++ FeedCaseStudyiOS/Feed UI/Composer/FeedUIComposer.swift
@@ -15,7 +15,7 @@ public final class FeedUIComposer {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(presenter: feedPresenter)
         let feedViewController = FeedViewController(refreshController: refreshController)
-        feedPresenter.loadingView = refreshController
+        feedPresenter.loadingView = WeakRefVirtualProxy(refreshController)
         feedPresenter.feedView = FeedAdapter(controller: feedViewController, loader: imageLoader)
         return feedViewController
     }
@@ -29,8 +29,22 @@ public final class FeedUIComposer {
     }
 }
 
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedRefreshViewController {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
+
 final class FeedAdapter: FeedView {
-    let controller: FeedViewController
+    weak var controller: FeedViewController?
     let loader: FeedImageDataLoader
     
     init(controller: FeedViewController, loader: FeedImageDataLoader) {
@@ -39,7 +53,7 @@ final class FeedAdapter: FeedView {
     }
     
     func display(feed: [FeedImage]) {
-        controller.tableModel = feed.map({ model in
+        controller?.tableModel = feed.map({ model in
             FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
         })
     }
