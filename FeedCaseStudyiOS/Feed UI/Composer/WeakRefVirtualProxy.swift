0ab2b6ebdae8bf7ee8b204f772edb3703f12e5ae
Andy Chang <andy.chang@ai3.cloud>
2022-11-03 22:59:05 +0800

    Move `WeakRefVirtualProxy` to separate file

---
+++ FeedCaseStudyiOS/Feed UI/Composer/WeakRefVirtualProxy.swift
@@ -0,0 +1,28 @@
+//
+//  WeakRefVirtualProxy.swift
+//  FeedCaseStudyiOS
+//
+//  Created by Andy Chang on 2022/11/3.
+//
+
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedRefreshViewController {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
