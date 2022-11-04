e09daef95a5cbff392f6c1e43173937f8510d29d
Andy Chang <andy.chang@ai3.cloud>
2022-11-04 14:03:34 +0800

    Display image with animation

---
+++ FeedCaseStudyiOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Animations.swift
+//  FeedCaseStudyiOS
+//
+//  Created by Andy Chang on 2022/11/4.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
