ca03413ea30ed8ce8acaf00f5cef3d4258e3dada
Andy Chang <andy.chang@ai3.cloud>
2022-11-10 22:05:16 +0800

    Move `ErrorView` to separate file

---
+++ FeedCaseStudyiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  FeedCaseStudyiOS
+//
+//  Created by Andy Chang on 2022/11/10.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
