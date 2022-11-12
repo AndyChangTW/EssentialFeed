ada5d9419cdc1c0b5531a63f47c76eb31d61cdc6
Andy Chang <andy.chang@ai3.cloud>
2022-11-12 21:17:03 +0800

    `FeedPresenter` displays no error on didStartLoadingFeed

--- Feed Case StudyTests/Feed Presentation/FeedPresenterTests.swift
+++ Feed Case StudyTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,29 @@
 
 import XCTest
 
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 private final class FeedPresenter {
-    init() {
-        
+    
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoading() {
+        errorView.display(.noError)
     }
 }
 
@@ -21,16 +41,32 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty)
     }
     
+    func test_didStartLoading_deliversNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoading()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     //MARK: Helpers
     private func makeSUT() -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter()
+        let sut = FeedPresenter(errorView: view)
         return (sut, view)
     }
     
     
-    private class ViewSpy {
+    private class ViewSpy: FeedErrorView {
+        var messages = [Message]()
         
-        var messages = [Any]()
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
+        
     }
 }
