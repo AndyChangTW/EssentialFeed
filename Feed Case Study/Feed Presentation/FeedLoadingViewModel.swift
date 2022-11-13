165b25556a91778eab03f20da78bf6d786b8592a
Andy Chang <andy.chang@ai3.cloud>
2022-11-13 12:15:38 +0800

    move view models to separate files

---
+++ Feed Case Study/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  FeedCaseStudy
+//
+//  Created by Andy Chang on 2022/11/13.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ Feed Case Study/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  FeedCaseStudyTests
+//
+//  Created by Andy Chang on 2022/11/13.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
