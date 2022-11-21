646dda7d771c740aa713e1f7b586b8eb9390f1a1
Andy Chang <andy.chang@ai3.cloud>
2022-11-21 22:49:01 +0800

    `FeedImagePresenter` displays retry on `didFinishLoadingImageDataWithError`

--- Feed Case StudyTests/Feed Presentation/FeedImagePresenterTests.swift
+++ Feed Case StudyTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -118,6 +118,22 @@ final class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(message?.shouldRetry, false)
     }
     
+    func test_didFinishLoadingImageDataWithError_displaysRetry() {
+        let feed = uniqueImage()
+        let error = anyNSError()
+        let (sut, view) = makeSUT()
+        
+        sut.didFinishLoadingImageData(with: error, for: feed)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, feed.description)
+        XCTAssertEqual(message?.location, feed.location)
+        XCTAssertNil(message?.image)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, true)
+    }
+    
     private func makeSUT(imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil }, file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, AnyImage>, view: ViewSpy) {
         let view = ViewSpy()
         let sut = FeedImagePresenter(view: view, imageTransformer: imageTransformer)
