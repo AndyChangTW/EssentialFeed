90567783b9d14f5efa6f76464498428bb821f89f
Andy Chang <andy.chang@ai3.cloud>
2022-11-21 22:53:55 +0800

    Move `FeedImageViewModel` to separate file.

---
+++ Feed Case Study/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModel.swift
+//  FeedCaseStudy
+//
+//  Created by Andy Chang on 2022/11/21.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
